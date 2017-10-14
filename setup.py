458a4b72bfa844752da9391771cf91043aa44392
Hugo <hugovk@users.noreply.github.com>
2017-10-14 11:51:34 +0300

    Update classifiers

--- setup.py
+++ setup.py
@@ -95,7 +95,11 @@ def main():
                        'Operating System :: POSIX',
                        'Programming Language :: Python',
                        'Programming Language :: Python :: 2',
+                       'Programming Language :: Python :: 2.7',
                        'Programming Language :: Python :: 3',
+                       'Programming Language :: Python :: 3.4',
+                       'Programming Language :: Python :: 3.5',
+                       'Programming Language :: Python :: 3.6',
                        'Topic :: Database',
                        ],
 
