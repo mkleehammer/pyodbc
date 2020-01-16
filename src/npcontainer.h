ec7aaa78641da6f74e52b4a3eb873e29170a48ca
Ilan Schnell <ilanschnell@gmail.com>
2020-01-15 22:11:26 -0600

    fix declaration of fetchdictarray_doc in npcontainer.h

--- src/npcontainer.h
+++ src/npcontainer.h
@@ -8,9 +8,9 @@ int NpContainer_init();
 void NpContainer_init();
 #endif
 
-PyObject* Cursor_fetchdictarray(PyObject* self, PyObject* args, PyObject *kwargs);
+PyObject *Cursor_fetchdictarray(PyObject *self, PyObject *args, PyObject *kwargs);
 
-extern char *fetchdictarray_doc;
+extern char fetchdictarray_doc[];
 
 extern Py_ssize_t iopro_text_limit;
 
