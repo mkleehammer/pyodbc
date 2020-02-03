5ccfcad007140449cbdc747a816a34cd7c6c2765
Keith Erskine <toastie604@gmail.com>
2020-02-03 17:49:18 -0600

    PYTHON_HOME instead of PYTHON

--- appveyor/after_test.cmd
+++ appveyor/after_test.cmd
@@ -1,10 +1,10 @@
 IF "%APVYR_GENERATE_WHEELS%" == "true" (
   ECHO *** pip install the "wheel" module
-  "%PYTHON%\python" -m pip install --upgrade pip --no-warn-script-location
-  "%PYTHON%\python" -m pip install wheel --no-warn-script-location
+  "%PYTHON_HOME%\python" -m pip install --upgrade pip --no-warn-script-location
+  "%PYTHON_HOME%\python" -m pip install wheel --no-warn-script-location
   ECHO.
   ECHO *** Generate the wheel file
-  "%WITH_COMPILER%" "%PYTHON%\python" setup.py bdist_wheel
+  "%WITH_COMPILER%" "%PYTHON_HOME%\python" setup.py bdist_wheel
   ECHO.
   ECHO *** \dist directory listing:
   DIR /B dist
