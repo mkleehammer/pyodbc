ad23de1ba2d9ec3445fe070dbd115b3d2c230c46
Keith Erskine <toastie604@gmail.com>
2020-05-24 10:25:25 -0500

    Change of truncation error for SQL Server 2019

--- tests2/sqlservertests.py
+++ tests2/sqlservertests.py
@@ -1544,7 +1544,8 @@ class SqlServerTestCase(unittest.TestCase):
         self.cursor.execute("create table t1(s varchar(800))")
         def test():
             self.cursor.execute("insert into t1 values (?)", value)
-        self.assertRaises(pyodbc.DataError, test)
+        # different versions of SQL Server generate different errors
+        self.assertRaises((pyodbc.DataError, pyodbc.ProgrammingError), test)
 
     def test_geometry_null_insert(self):
         def convert(value):
