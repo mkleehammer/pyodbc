e4b971d21d4cd92ee980c03fe893272b68b5a817
Steven James <sjames@jacksonfurnind.com>
2020-06-17 13:38:04 -0400

    fix typos

--- src/getdata.cpp
+++ src/getdata.cpp
@@ -650,9 +650,9 @@ static PyObject* GetDataTimestamp(Cursor* cur, Py_ssize_t iCol)
         t.tm_hour = value.hour; t.tm_min = value.minute; t.tm_sec = value.second;
         t.tm_isdst = -1; // auto-adjust for dst
 
-        mktime(t); // normalize values in t
+        mktime(&t); // normalize values in t
         return PyDateTime_FromDateAndTime(
-            t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm.min, t.tm_sec, micros
+            t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec, micros
         );
     }
 
