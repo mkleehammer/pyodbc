8d6120b2f3e6c502d042d6647bae7efdc1a60aa1
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-09-12 18:37:21 +0300

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -110,6 +110,8 @@ def main():
                        'Programming Language :: Python :: 3.6',
                        'Programming Language :: Python :: 3.7',
                        'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
+                       'Programming Language :: Python :: 3.10',
                        'Topic :: Database',
                        ],
 
