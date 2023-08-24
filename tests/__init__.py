e464f66fa201023750be974c5d48f47879a91ec6
Michael Kleehammer <michael@kleehammer.com>
2023-08-24 16:47:06 -0500

    Add tests.__init__.py back
    I accidentally deleted it.  It is required for simple local pytest.  (See comment in the file.)

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+# This file is required to simplify running pytest.
+#
+# Build pyodbc into the project root by running: python setup.py build_ext --inplace
+#
+# Then run pytest from the root: pytest tests/postgresql_test.py
