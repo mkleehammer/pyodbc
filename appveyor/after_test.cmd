3ec1782a2e91a2e40b919fe0ffe4e240d4af7080
Keith Erskine <toastie604@gmail.com>
2023-08-26 19:40:19 -0500

    don't call setup.py directly

--- appveyor/after_test.cmd
+++ appveyor/after_test.cmd
@@ -1,12 +1,13 @@
 IF "%APVYR_GENERATE_WHEELS%" == "true" (
-  ECHO *** pip install the "wheel" module
-  "%PYTHON_HOME%\python" -m pip install wheel --quiet --no-warn-script-location
+  ECHO *** pip install build/wheel modules
+  "%PYTHON_HOME%\python" -m pip install build wheel --quiet --no-warn-script-location
   ECHO.
   ECHO *** Generate the wheel file
-  %WITH_COMPILER% "%PYTHON_HOME%\python" setup.py bdist_wheel
+  %WITH_COMPILER% "%PYTHON_HOME%\python" -m build --wheel --no-isolation
   ECHO.
   ECHO *** \dist directory listing:
   DIR /B dist
+  ECHO.
 ) ELSE (
   ECHO *** Skipping generation of the wheel file
   ECHO.
--- appveyor/install.cmd
+++ appveyor/install.cmd
@@ -1,2 +1,3 @@
 ECHO *** pip install pytest and other dev requirements ***
 "%PYTHON_HOME%\python" -m pip install -r requirements-dev.txt
+ECHO.
