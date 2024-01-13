988882a099fc8a3e83803164b6bc985795d574a8
ffelixg <142172984+ffelixg@users.noreply.github.com>
2024-01-13 18:24:54 +0100

    debug

--- src/getdata.cpp
+++ src/getdata.cpp
@@ -994,6 +994,7 @@ bool BindCol(Cursor* cur, Py_ssize_t iCol)
         return true;
     }
 
+    size = size * 4 + 100;
     cur->valueBufs[iCol] = PyMem_Malloc(size);
     if (!cur->valueBufs[iCol])
     {
