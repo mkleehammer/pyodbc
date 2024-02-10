5a062e81e1c5ae7217881b44e915e136514126df
Keith Erskine <toastie604@gmail.com>
2024-02-10 07:46:13 -0600

    remove redundant compile.cmd script
    It used to be necessary to have special DOS compilation commands when compiling earlier versions of pyodbc but that is no longer needed.  Besides, we build Windows wheels in Github Actions now.

--- appveyor/after_test.cmd
+++ appveyor/after_test.cmd
@@ -3,7 +3,7 @@ IF "%APVYR_GENERATE_WHEELS%" == "true" (
   "%PYTHON_HOME%\python" -m pip install build wheel --quiet --no-warn-script-location
   ECHO.
   ECHO *** Generate the wheel file
-  %WITH_COMPILER% "%PYTHON_HOME%\python" -m build --wheel --no-isolation
+  "%PYTHON_HOME%\python" -m build --wheel --no-isolation
   ECHO.
   ECHO *** \dist directory listing:
   DIR /B dist
